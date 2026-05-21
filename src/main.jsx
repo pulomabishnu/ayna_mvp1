7205ab0835c4a82727eae4e6f8eb06cf4602ff20
Puloma Bishnu <pulomabishnu@gmail.com>
2026-05-21 16:40:14 -0400

    add posthog

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import posthog from 'posthog-js'
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  person_profiles: 'identified_only',
+})
 
 class ErrorBoundary extends React.Component {
   state = { hasError: false, error: null }
