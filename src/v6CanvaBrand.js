8882709851cb346416754a8d744247173b65b463
ameera-aynahealth <ameera@aynahealth.co>
2026-09-14 09:24:06 -0400

    Use one exact Canva skin over original layouts

--- src/v6CanvaBrand.js
+++ src/v6CanvaBrand.js
@@ -1,7 +1,1 @@
-import './v6CanvaBrand.css';
-import './v6CanvaHome.css';
-import './v6CanvaOverrides.css';
-import './v6CanvaTokens.css';
-import './v6CanvaDarkMode.css';
-import './v6CanvaSkinOnly.css';
-import './v6CanvaProductCards.css';
+import './v6CanvaExactSkin.css';
